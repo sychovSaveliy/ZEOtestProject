1e3ba19a54e6e49cf74906c1ea80a366735804dd
savapond <sychov.saveliy@gmail.com>
2016-10-29 13:28:10 +0300

    create header, add font..and gitignore not working:)

--- config.rb
+++ config.rb
@@ -5,5 +5,5 @@ sass_dir = 'assets/sass'
 images_dir = 'assets/images'
 javascripts_dir = 'assets/js'
 relative_assets = true
-line_comments = true
+line_comments = false
 # output_style = :compressed
