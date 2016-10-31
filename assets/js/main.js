25f45bada340da3a53a3cca7927c0754e7776e97
savapond <sychov.saveliy@gmail.com>
2016-10-31 23:22:19 +0200

    create tool-icon layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	
 	
 	$(".svg-cont use").on("mouseover", function(e){
-		$(this).attr({"stroke-width":"4"});
+		$(this).attr({"stroke-width":"4","stroke":"#00a4ff"});
 	});
 	
 	$(".svg-cont use").on("mouseout", function(e){
-		$(this).attr({"stroke-width":"1"});
+		$(this).attr({"stroke-width":"1","stroke":"#cecece"});
 	});
 	
 });
